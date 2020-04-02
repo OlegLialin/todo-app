7dfc90698b9c5c9d9857fd75403292635d282d3b
olegliallin <olegliallin@gmail.com>
2020-04-02 21:44:01 +0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, aut consequatur exercitationem facilis laborum
+      minima natus pariatur rem tenetur veritatis?</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
---
+++ src/views/Todos.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <h2>Todo list</h2>
+    <router-link to="/">Home</router-link>
+    <hr>
+    <AddToDo @add-todo="addTodo"/>
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="not-completed">Not Completed</option>
+    </select>
+    <hr>
+    <Loader v-if="loading"/>
+    <TodoList
+        v-else-if="filteredTodos.length"
+        :todos="filteredTodos"
+        @remove-todo="removeTodo"
+    />
+    <p v-else>No todos!</p>
+  </div>
+</template>
+
+<script>
+  import TodoList from '@/components/TodoList'
+  import AddToDo from '@/components/AddToDo'
+  import Loader from '@/components/Loader'
+
+  export default {
+    name: "app",
+    data() {
+      return {
+        todos: [],
+        loading: true,
+        filter: 'all'
+      }
+    },
+    mounted() {
+      fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+        .then(response => response.json())
+        .then(json => {
+          setTimeout(() => {
+            this.todos = json
+            this.loading = false
+          }, 1000)
+        })
+    },
+    computed: {
+      filteredTodos() {
+        if (this.filter === 'all') {
+          return this.todos
+        }
+
+        if (this.filter === 'completed') {
+          return this.todos.filter(t => t.completed)
+        }
+
+        if (this.filter === 'not-completed') {
+          return this.todos.filter(t => !t.completed)
+        }
+        return this.todos
+      }
+    },
+    methods: {
+      removeTodo(id) {
+        this.todos = this.todos.filter(t => t.id !== id)
+      },
+      addTodo(newTodo) {
+        this.todos.push(newTodo)
+      }
+    },
+    components: {
+      TodoList,
+      AddToDo,
+      Loader
+    }
+  };
+</script>
